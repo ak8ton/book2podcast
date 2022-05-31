5ccf61e44b79c27826580c83078b9df43301008f
Anton <ak8ton@gmail.com>
2022-05-31 21:45:42 +0500

    Refactoring

--- rss.go
+++ rss.go
@@ -17,27 +17,11 @@ import (
 )
 
 //go:embed index.html
-var s string
+var htmlTemplate string
+
+var pageTemplate *template.Template
 
 const timeFormat = "20060102150405"
-
-/*
-func logHtml(doc *html.Node) {
-	if doc == nil {
-		return
-	}
-
-	var searchLinks func(*html.Node, int)
-	searchLinks = func(node *html.Node, indent int) {
-		fmt.Printf("[%d] %d: ", indent, node.Type)
-		fmt.Printf("%s\n", node.Data)
-		for child := node.FirstChild; child != nil; child = child.NextSibling {
-			searchLinks(child, indent+1)
-		}
-	}
-	searchLinks(doc, 0)
-}
-*/
 
 func getText(node *html.Node) string {
 	if node != nil {
@@ -81,7 +65,30 @@ func match(pattern string, url *url.URL) bool {
 	return false
 }
 
-func getAllLinks(w http.ResponseWriter, pattern string, doc *html.Node, baseUrl *url.URL) {
+func writeLink(w http.ResponseWriter, fileName string, url string, mimeType string) {
+	fmt.Fprintf(w, "<item>")
+	fmt.Fprintf(w, "<title>%s</title>", fileName)
+	fmt.Fprintf(w, "<enclosure url=\"%s\" type=\"%s\"/>", url, mimeType)
+	fmt.Fprintf(w, "</item>")
+}
+
+func getFileName(urlPath string, linkText string) (fileName string, mimeType string) {
+	mimeType = ""
+	fileName = filepath.Base(urlPath)
+	extension := filepath.Ext(fileName)
+	if linkText != "" {
+		fileName = linkText
+	}
+
+	if extension != "" {
+		mimeType = mime.TypeByExtension(extension)
+		fileName = strings.TrimSuffix(fileName, extension)
+	}
+
+	return fileName, mimeType
+}
+
+func writeAllLinks(w http.ResponseWriter, pattern string, doc *html.Node, baseUrl *url.URL) {
 	if doc == nil {
 		return
 	}
@@ -103,24 +110,9 @@ func getAllLinks(w http.ResponseWriter, pattern string, doc *html.Node, baseUrl 
 					}
 
 					if match(pattern, absUrl) {
-						mimeType := ""
-						fileName := filepath.Base(absUrl.Path)
-						extension := filepath.Ext(fileName)
-						filepath.Glob("")
 						linkText := getText(node)
-						if linkText != "" {
-							fileName = linkText
-						}
-
-						if extension != "" {
-							mimeType = mime.TypeByExtension(extension)
-							fileName = strings.TrimSuffix(fileName, extension)
-						}
-
-						fmt.Fprintf(w, "<item>")
-						fmt.Fprintf(w, "<title>%s</title>", fileName)
-						fmt.Fprintf(w, "<enclosure url=\"%s\" type=\"%s\"/>", absUrl.String(), mimeType)
-						fmt.Fprintf(w, "</item>")
+						fileName, mimeType := getFileName(absUrl.Path, linkText)
+						writeLink(w, fileName, absUrl.String(), mimeType)
 					}
 				}
 			}
@@ -130,6 +122,27 @@ func getAllLinks(w http.ResponseWriter, pattern string, doc *html.Node, baseUrl 
 		}
 	}
 	searchLinks(doc)
+}
+
+func writeRss(_doc *html.Node, w http.ResponseWriter, pattern string, baseUrl *url.URL) {
+	htmlNode := getChild(_doc, "html")
+
+	fmt.Fprintf(w, "<?xml version='1.0' encoding='UTF-8' ?>")
+	fmt.Fprintf(w, "<rss version='2.0'>")
+	fmt.Fprintf(w, "<channel>")
+
+	title := "Book"
+	pageTitle := getText(getChild(getChild(htmlNode, "head"), "title"))
+	if pageTitle != "" {
+		title = pageTitle
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("<title>%s</title>", title))
+
+	writeAllLinks(w, pattern, getChild(htmlNode, "body"), baseUrl)
+
+	fmt.Fprintf(w, "</channel>")
+	fmt.Fprintf(w, "</rss>")
 }
 
 func httpGet(url string) (*html.Node, *url.URL, error) {
@@ -176,26 +189,7 @@ func rssHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// logHtml(doc)
-	doc = getChild(doc, "html")
-	// log.Fatal("html")
-
-	fmt.Fprintf(w, "<?xml version='1.0' encoding='UTF-8' ?>")
-	fmt.Fprintf(w, "<rss version='2.0'>")
-	fmt.Fprintf(w, "<channel>")
-
-	title := "Book"
-	pageTitle := getText(getChild(getChild(doc, "head"), "title"))
-	if pageTitle != "" {
-		title = pageTitle
-	}
-
-	fmt.Fprintf(w, fmt.Sprintf("<title>%s</title>", title))
-
-	getAllLinks(w, pattern, getChild(doc, "body"), baseUrl)
-
-	fmt.Fprintf(w, "</channel>")
-	fmt.Fprintf(w, "</rss>")
+	writeRss(doc, w, pattern, baseUrl)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -204,7 +198,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 			http.StatusNotFound)
 		return
 	}
-	err := t.Execute(w, time.Now().Format(timeFormat))
+	err := pageTemplate.Execute(w, time.Now().Format(timeFormat))
 	logError("Template.Execute", err)
 }
 
@@ -213,8 +207,6 @@ func logError(trace string, err error) {
 		log.Println(trace+": %s", err)
 	}
 }
-
-var t *template.Template
 
 func addExtensionType(extension, mimeType string) {
 	err := mime.AddExtensionType(extension, mimeType)
@@ -230,7 +222,7 @@ func main() {
 	addExtensionType(".mov", "video/quicktime")
 
 	var err error = nil
-	t, err = template.New("indexPage").Parse(s)
+	pageTemplate, err = template.New("indexPage").Parse(htmlTemplate)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -239,5 +231,5 @@ func main() {
 	http.HandleFunc("/feed", rssHandler)
 
 	err = http.ListenAndServe(":8080", nil)
-	logError("ListenAndServe", err)
+	log.Fatal(err)
 }
